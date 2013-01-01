e2b7e34687759557b2e2f79d0787dddc1d7290a9
Dario Seminara <darios3@gmail.com>
2013-01-01 14:20:37 -0300

    lisp.lsp: +1 failing test: simple evaluation of "(car '(1 2 3))"

---
+++ lisp/spec.rb
@@ -0,0 +1,18 @@
+require "dslisprb"
+
+describe DsLisp, "N queens" do
+  def lisp
+    dslisp = DsLisp.new
+    code = File.open("lisp.lsp").read.gsub("\n"," ")
+    dslisp.evaluate(code)
+    dslisp
+  end
+
+  def self.test_expression(expr)
+    it "should execute #{expr}" do 
+      lisp.evaluate("(lisp '#{expr})").should be == lisp.evaluate(expr);
+    end
+  end
+
+  test_expression("(car '(1 2 3))")
+end
