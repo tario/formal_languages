edc2b9e520d290ad76247225832e53c81222c3f7
Dario Seminara <darios3@gmail.com>
2013-01-06 06:03:43 -0300

    c: +1 failing test: simple declaration of variable

---
+++ c/spec.rb
@@ -0,0 +1,14 @@
+require "dslisprb"
+
+describe DsLisp, "C" do
+  def lisp
+    dslisp = DsLisp.new
+    code = "(block " + File.open("c.lsp").read.gsub("\n"," ") + " )"
+    dslisp.evaluate(code)
+    dslisp
+  end
+
+  it "should read simple variable declaration" do 
+    lisp.evaluate("(declaracion '(int A))").should be == [[:A, nil]];
+  end
+end
