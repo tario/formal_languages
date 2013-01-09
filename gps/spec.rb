9b17a79743b2d790ceca4150d700bf363f4e29d5
Dario Seminara <darios3@gmail.com>
2013-01-09 19:18:34 -0300

    gps: +5 failing test: graph using another format

--- gps/spec.rb
+++ gps/spec.rb
@@ -31,4 +31,24 @@ describe DsLisp, "GPS" do
   it "should solve two step path (1 2 3), with alternate paths" do
     lisp.evaluate("(camino_minimo '(1 2 3 4 5) '((1 2) (2 4) (4 5) (5 3) (2 3)) 1 3)").should be == [1, 2, 3]
   end
+
+  it "should solve single path" do
+    lisp.evaluate("(camino '((1 (2))) 1 2)").should be == [1, 2]
+  end
+
+  it "should solve alternate single path (1 3)" do
+    lisp.evaluate("(camino '((1 (3))) 1 3)").should be == [1, 3]
+  end
+
+  it "should solve two step path (1 2 3)" do
+    lisp.evaluate("(camino '((1 (2)) (2 (3))) 1 3)").should be == [1, 2, 3]
+  end
+
+  it "should solve two step path (1 2 3), with loops" do
+    lisp.evaluate("(camino '((1 (2)) (2 (4 3 5)) (4 (1))) 1 3)").should be == [1, 2, 3]
+  end
+
+  it "should solve two step path (1 2 3), with alternate paths" do
+    lisp.evaluate("(camino '((1 (2)) (2 (3 4)) (4 (5)) (5 (3))) 1 3)").should be == [1, 2, 3]
+  end
 end
